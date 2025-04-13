5743883a13836ad11c4450df9122f1675852d158
Lamiee <lambph1204@gmail.com>
2025-04-13 12:24:13 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
-    react()
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets',
+          dest: '' // Copy vào public/assets/
+        }
+      ]
+    })
   ],
   resolve: {
     alias: [
